5d7ee0f52d7e1a8c371ef08abd3a84293c9b96d9
hebu <2rwin_rommel@naver.com>
2025-04-09 10:35:29 +0900

    fetch_test 완료

--- TODO.md
+++ TODO.md
@@ -1,7 +1,30 @@
 # 250407
 
+```md
 카페24 토큰 매니저 만들기
 임시 상품 데이터 긁어오고 검증하기
 저장해야 할 데이터 정의하기
 긁어온 데이터 검증하기
 데이터베이스에 인서트하기
+```
+
+# 250409
+```md
+fetch 할 데이터 정의
+
+- platform
+- seller_id
+- product_id
+- company 
+- sale_name
+- product_name
+- data
+
+platform은 하드코딩
+seller_id는 유동적으로 가져오게
+
+scripts에 cafe24datamanager 만들어서 class로 관리하기
+data fetch, insert 로직까지 만들면 될듯..?
+
+db 정의하고 alembic init부터 하자.
+```
--- app/tests/fetch_test.py
+++ app/tests/fetch_test.py
@@ -1,6 +1,10 @@
 from app.scripts.cafe24tokenmanager import TokenManager
+from app.utils.logger import mainLogger
 import requests
 import json
+import os
+
+logger = mainLogger()
 
 def call_request(info='products/count'):
 
@@ -23,44 +27,50 @@ def call_request(info='products/count'):
 def get_all_products():
     token = TokenManager()
     access_token = token.get_access_token()
-    print(f'access_token = {access_token}')
-    
+
+    logger.info(f"엑세스 토큰: {access_token}")
+
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'
     }
-    
+
     all_products = []
-    page = 1
-    limit = 100  # 한 페이지당 조회할 상품 수
+    since_product_no = 1
+    limit = 100
+
     while True:
-        info = f'products?fields=product_no,product_code&page={page}&limit={limit}'
+        info = f'products?fields=product_no,product_code&since_product_no={since_product_no}&limit={limit}'
         url = f'https://richcp.cafe24api.com/api/v2/admin/{info}'
         response = requests.get(url, headers=headers)
-        
-        # 응답 본문이 비어 있거나 HTTP 에러이면 루프 종료
+
         if response.status_code != 200 or not response.text.strip():
-            print(f"페이지 {page}에서 응답이 비어 있습니다. 종료합니다.")
+            logger.error('응답이 비어 있습니다. 종료합니다.')
             break
-        
+
         try:
-            data = response.json()  # 응답 JSON 파싱
+            data = response.json()
         except json.JSONDecodeError:
-            print(f"페이지 {page}에서 JSON 파싱 실패. 종료합니다.")
-            break
-        
-        products = data.get('products', [])
+            logger.error('JSON 파싱 실패. 종료합니다.')
+
+        products = data.get('products',[])
+
+        num_products = len(products)
         all_products.extend(products)
-        print(f"페이지 {page} 조회, 제품 수: {len(products)}")
-        
-        # 만약 응답받은 상품 수가 limit보다 작다면 마지막 페이지로 판단
-        if len(products) < limit:
+
+        logger.info(f'{since_product_no} 조회, 상품 수: {num_products}')
+
+        if num_products < limit:
             break
-        page += 1
+
+        since_product_no += limit
 
     pretty_json = json.dumps(all_products, indent=4, ensure_ascii=False)
-    print(pretty_json)
-    
+    filename = 'products_output.json'
+
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(all_products, json_file, indent=4)
+
     return all_products
 
 get_all_products()
