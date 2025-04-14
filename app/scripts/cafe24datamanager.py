c790cd8c0b5b80aef07a9d664bf1a2125ac69bdb
hebu <2rwin_rommel@naver.com>
2025-04-14 10:40:06 +0900

    define_product_name 만드는중3

--- app/scripts/cafe24datamanager.py
+++ app/scripts/cafe24datamanager.py
@@ -89,7 +89,7 @@ class Cafe24DataManager():
                 'category': self.select_category(), 
                 'company': input('회사명을 입력해주세요: '),
                 'sale_name': product['product_name'],
-                'product_name': input('관리제품명을 입력해주세요: '),
+                'product_name': self.define_product_name(product['category']),
                 'data': product,
                 'created_at': datetime.datetime.utcnow(),
                 'updated_at': datetime.datetime.utcnow()
@@ -131,6 +131,7 @@ class Cafe24DataManager():
             except ValueError:
                 logger.info('숫자만 입력 가능합니다.')
     
+
     def define_product_name(self, category: str):
         """
         제품 데이터를 기반으로 관리 제품명을 정의하는 메소드입니다.
@@ -139,14 +140,20 @@ class Cafe24DataManager():
         Args:
             category (str): 대분류 카테고리
         Returns:
-            product_name (str): 관리제품명
+            full_name (str): 관리제품명
         """
-        product_name = input('상품명을 입력해주세요: ')
+        
+        base_name = input('기본 제품명을 입력해주세요: ')
+        full_name = base_name
+
+        # 액상 카테고리
         if category == '액상':
             sub_categories = ['입호흡액상', '폐호흡액상', '기타액상']
+            logger.info('--------------------------------')
             logger.info('액상 카테고리 소분류 목록:')
             for idx, sub_category in enumerate(sub_categories, 1):
                 logger.info(f'{idx}. {sub_category}')
+            logger.info('--------------------------------')
             sub_choice = input('소분류 번호를 입력해주세요: ')
             if sub_choice.isdigit():
                 idx = int(sub_choice)
@@ -157,27 +164,243 @@ class Cafe24DataManager():
             else:
                 logger.info('숫자만 입력 가능합니다.')
 
-        # if category == '기기':
 
-        # if category == '무화기':
+            # 액상 용량 선택
+            volume_options = ['60ml', '30ml', '100ml', '직접입력']
+            logger.info('--------------------------------')
+            logger.info('액상 카테고리 옵션명 목록:')
+            for idx, volume_option in enumerate(volume_options, 1):
+                logger.info(f'{idx}. {volume_option}')
+            logger.info('--------------------------------')
+            volume_choice = input('옵션명을 입력해주세요: ')
+            if volume_choice.isdigit():
+                idx = int(volume_choice)
+                if 1 <= idx <= len(volume_options):
+                    volume_option = volume_options[idx - 1]
+                else:
+                    logger.info('올바른 옵션명 번호를 입력해주세요.')
+            else:
+                logger.info('숫자만 입력 가능합니다.')  
 
-        # if category == '코일':
+            # 액상 니코틴 함유량 선택
+            if volume_option == '직접입력':
+                volume_option = input('액상 용량을 입력해주세요: ')  
+            
+            nicotine_options = ['3mg', '6mg', '9mg', '무니코틴', '직접입력']
+            logger.info('--------------------------------')
+            logger.info('액상 카테고리 옵션명 목록:')
+            for idx, nicotine_option in enumerate(nicotine_options, 1):
+                logger.info(f'{idx}. {nicotine_option}')
+            logger.info('--------------------------------')
+            nicotine_choice = input('옵션명을 입력해주세요: ')
+            if nicotine_choice.isdigit():
+                idx = int(nicotine_choice)
+                if 1 <= idx <= len(nicotine_options):
+                    nicotine_option = nicotine_options[idx - 1]
+                else:
+                    logger.info('올바른 옵션명 번호를 입력해주세요.')
+            else:
+                logger.info('숫자만 입력 가능합니다.')
 
-        # if category == '팟':
+            if nicotine_option == '직접입력':
+                nicotine_option = input('니코틴 함유량을 입력해주세요: ')
 
-        # if category == '일회용기기':
+            if 'sub_category' in locals():
+                full_name += f'|{sub_category}'
 
-        # if category == '악세사리':
+            if 'volume_option' in locals():
+                full_name += f'|{volume_option}'
 
-        # if category == '기타':
+            if 'nicotine_option' in locals() and nicotine_option.strip():
+                full_name += f'|{nicotine_option}'
+                
+                
+        # 기기 카테고리
+        if category == '기기':
+            sub_categories = ['입호흡기기', '폐호흡기기', 'AIO기기', '기타기기']
+            logger.info('--------------------------------')
+            logger.info('기기 카테고리 소분류 목록:')
+            for idx, sub_category in enumerate(sub_categories, 1):
+                logger.info(f'{idx}. {sub_category}')
+            logger.info('--------------------------------')
+            sub_choice = input('소분류 번호를 입력해주세요: ')
+            if sub_choice.isdigit():
+                idx = int(sub_choice)
+                if 1 <= idx <= len(sub_categories):
+                    sub_category = sub_categories[idx - 1]
+                else:
+                    logger.info('올바른 소분류 번호를 입력해주세요.')
+            else:
+                logger.info('숫자만 입력 가능합니다.')
 
-        option_name = input('옵션명을 입력해주세요 (생략 가능): ')
+            # 기기 옵션명
+            sub_options = input('옵션명을 입력해주세요(생략 가능): ')
 
-        if sub_category.strip():
-            full_name += '|' + sub_category.strip()
+            if 'sub_category' in locals():
+                full_name += f'|{sub_category}'
 
-        if option_name.strip():
-            full_name += '|' + option_name.strip()
+            if 'sub_options' in locals() and sub_options.strip():
+                full_name += f'|{sub_options.strip()}'
+
+        # 무화기 카테고리
+        if category == '무화기':
+            sub_categories = ['RDA', 'RTA', 'RDTA', '기성탱크', '팟탱크', 'AIO팟', '일회용무화기', '기타무화기']
+            logger.info('--------------------------------')
+            logger.info('무화기 카테고리 소분류 목록:')
+            for idx, sub_category in enumerate(sub_categories, 1):
+                logger.info(f'{idx}. {sub_category}')
+            logger.info('--------------------------------')
+            sub_choice = input('소분류 번호를 입력해주세요: ')
+            if sub_choice.isdigit():
+                idx = int(sub_choice)
+                if 1 <= idx <= len(sub_categories):
+                    sub_category = sub_categories[idx - 1]
+                else:
+                    logger.info('올바른 소분류 번호를 입력해주세요.')
+
+            # 무화기 옵션명
+            sub_options = ['입호흡무화기', '폐호흡무화기']
+            logger.info('--------------------------------')
+            logger.info('무화기 카테고리 옵션명 목록:')
+            for idx, sub_option in enumerate(sub_options, 1):
+                logger.info(f'{idx}. {sub_option}')
+            logger.info('--------------------------------')
+            sub_option_choice = input('옵션명을 입력해주세요: ')
+            if sub_option_choice.isdigit():
+                idx = int(sub_option_choice)
+                if 1 <= idx <= len(sub_options):
+                    sub_option = sub_options[idx - 1]
+                else:
+                    logger.info('올바른 옵션명 번호를 입력해주세요.')
+            else:
+                logger.info('숫자만 입력 가능합니다.')
+
+            # 무화기 세부 옵션명
+            second_sub_options = input('세부 옵션명을 입력해주세요(생략 가능): ')
+
+            if 'sub_category' in locals():
+                full_name += f'|{sub_category}'
+
+            if 'sub_option' in locals():
+                full_name += f'|{sub_option}'
+
+            if 'second_sub_options' in locals() and second_sub_options.strip():
+                full_name += f'|{second_sub_options.strip()}'
+
+        # 코일 카테고리
+        if category == '코일':
+            sub_option_choice = input('옵션명을 입력해주세요(생략 가능): ')
+
+            if 'sub_option_choice' in locals() and sub_option_choice.strip():
+                full_name += f'|코일|{sub_option_choice.strip()}'
+
+        # 팟 카테고리
+        if category == '팟':
+            sub_options = ['일체형팟', '공팟', '기타팟']
+            logger.info('--------------------------------')
+            logger.info('팟 카테고리 옵션명 목록:')
+            for idx, sub_option in enumerate(sub_options, 1):
+                logger.info(f'{idx}. {sub_option}')
+            logger.info('--------------------------------')
+            
+            sub_option_choice = input('옵션명을 입력해주세요: ')
+            if sub_option_choice.isdigit():
+                idx = int(sub_option_choice)
+                if 1 <= idx <= len(sub_options):
+                    sub_option = sub_options[idx - 1]
+                else:
+                    logger.info('올바른 옵션명 번호를 입력해주세요.')   
+            else:
+                logger.info('숫자만 입력 가능합니다.')
+
+            ohm_option = input('옴 옵션을 입력해주세요(생략 가능): ')
+
+            if 'sub_option' in locals():
+                full_name += f'|{sub_option}'
+            
+            if 'ohm_option' in locals() and ohm_option.strip():
+                full_name += f'{ohm_option.strip()}'
+
+        if category == '일회용기기':
+            sub_options = ['일체형', '교체형', '무니코틴', '기타일회용기기']
+            logger.info('--------------------------------')
+            logger.info('일회용기기 카테고리 옵션명 목록:')
+            for idx, sub_option in enumerate(sub_options, 1):
+                logger.info(f'{idx}. {sub_option}')
+            logger.info('--------------------------------')
+            
+            sub_option_choice = input('옵션명을 입력해주세요: ')
+            if sub_option_choice.isdigit():
+                idx = int(sub_option_choice)
+                if 1 <= idx <= len(sub_options):
+                    sub_option = sub_options[idx - 1]
+                else:
+                    logger.info('올바른 옵션명 번호를 입력해주세요.')   
+            else:
+                logger.info('숫자만 입력 가능합니다.')
+            
+            if sub_option == '교체형':
+                detail_option = ['배터리', '카트리지']
+                logger.info('--------------------------------')
+                logger.info('교체형기기 카테고리 세부 옵션명 목록:')
+                for idx, detail_option in enumerate(detail_option, 1):
+                    logger.info(f'{idx}. {detail_option}')
+                logger.info('--------------------------------')
+
+                detail_option_choice = input('세부 옵션명을 입력해주세요: ')
+                if detail_option_choice.isdigit():
+                    idx = int(detail_option_choice)
+                    if 1 <= idx <= len(detail_option):
+                        detail_option = detail_option[idx - 1]
+                    else:
+                        logger.info('올바른 세부 옵션명 번호를 입력해주세요.')
+                else:
+                    logger.info('숫자만 입력 가능합니다.')
+            
+            detail_option = input('세부 옵션명을 입력해주세요(생략 가능): ')
+
+            if 'sub_option' in locals():
+                full_name += f'|{sub_option}'
+
+            if 'detail_option' in locals() and detail_option.strip():
+                full_name += f'|{detail_option.strip()}'
+
+
+        if category == '악세사리':
+            sub_options = ['경통', '드립팁', '캡', '케이스', '도어', '배터리', '충전기', '리빌드용품', '기타악세사리']
+            logger.info('--------------------------------')
+            logger.info('악세사리 카테고리 옵션명 목록:')
+            for idx, sub_option in enumerate(sub_options, 1):
+                logger.info(f'{idx}. {sub_option}')
+            logger.info('--------------------------------')
+
+            sub_option_choice = input('옵션명을 입력해주세요: ')
+            if sub_option_choice.isdigit():
+                idx = int(sub_option_choice)
+                if 1 <= idx <= len(sub_options):
+                    sub_option = sub_options[idx - 1]
+                else:
+                    logger.info('올바른 옵션명 번호를 입력해주세요.')
+            else:
+                logger.info('숫자만 입력 가능합니다.')
+
+            sub_detail_option = input('세부 옵션명을 입력해주세요(생략 가능): ')
+
+            if 'sub_option' in locals():
+                full_name += f'|{sub_option}'
+
+            if 'sub_detail_option' in locals() and sub_detail_option.strip():
+                full_name += f'|{sub_detail_option.strip()}'
+
+        if category == '기타':
+            sub_option = '기타'
+
+            sub_detail_option = input('세부 옵션명을 입력해주세요(생략 가능): ')
+
+            if sub_option in locals():
+                full_name += f'|{sub_option}'
+
+            if sub_detail_option in locals() and sub_detail_option.strip():
+                full_name += f'|{sub_detail_option.strip()}'
 
         return full_name
-
