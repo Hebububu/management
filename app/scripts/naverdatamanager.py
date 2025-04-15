7ae97c2abf3b5a552cd7c8bb0db32ce0d0ab5ac2
hebu <2rwin_rommel@naver.com>
2025-04-15 17:44:11 +0900

    - sort_product_data (NaverDataManager)

--- app/scripts/naverdatamanager.py
+++ app/scripts/naverdatamanager.py
@@ -1,6 +1,7 @@
 from app.utils.logger import mainLogger
 from app.scripts.navertokenmanager import NaverTokenManager
 from app.database.crud.product_crud import ProductCRUD
+import datetime
 import requests
 import json
 
@@ -69,11 +70,11 @@ class NaverDataManager:
         return all_products_list
             
 
-    def get_product_data(self, channel_product_numbers: list):
+    def get_product_data(self, all_products_list: list):
         """
-        channel_product_numbers를 기반으로 네이버커머스 제품 데이터를 가져오는 메소드입니다.
+        all_products_list를 기반으로 네이버커머스 제품 데이터를 가져오는 메소드입니다.
         Args:
-            channel_product_numbers (list): 네이버커머스 제품 ID 리스트
+            all_products_list (list): 네이버커머스 제품 ID 리스트
         Returns:
             product_data (list): 네이버커머스 제품 데이터
         """
@@ -81,7 +82,7 @@ class NaverDataManager:
 
         product_data = []
 
-        for channel_product_number in channel_product_numbers:
+        for channel_product_number in all_products_list:
             url = f'{self.url}{channel_product_number}'
 
             payload = {}
@@ -99,18 +100,45 @@ class NaverDataManager:
         return product_data
             
 
-    def sort_product_data(self, product_data: dict):
+    def sort_product_data(self, product_data: dict, seller_id: str):
         """
         DB 저장에 필요한 정보를 추출하고 객체로 만드는 메소드입니다.
         Args:
             product_data (dict): 네이버커머스 제품 데이터
+            seller_id (str): 판매자 ID
         Returns:
             sorted_product_data (dict): DB 저장에 필요한 정보를 추출한 제품 데이터
         """
 
-        
+        sorted_product_data = []
+
+        for product in product_data:
+            logger.info(f'제품명: {product["example_name"]}')
+
+            product_data = {
+                'platform': 'naver',
+                'seller_id': seller_id,
+                'product_id': product['channel_product_number'],
+                'category': None,
+                'company': None,
+                'sale_name': product['example_name'],
+                'product_name': None,
+                'tags': None,
+                'data': product,
+                'created_at': datetime.datetime.utcnow(),
+                'updated_at': datetime.datetime.utcnow()
+            }
+
+            if product_data['example_name'] == '':
+                logger.info(f'관리제품명이 입력되지 않았습니다. 기존 제품명을 사용합니다.')
+                product_data['product_name'] = product['example_name']
+
+            sorted_product_data.append(product_data)
+
+        return sorted_product_data
 
 
+    
     #def select_category(self)
     #굳이 안만들고 그냥 저기서 재활용해도 될거같은데 생각해봄 
 
